05c47d15c58590dac17daa606d84bf20692adbb5
Sam Stanton-Reid <stantonr@bu.edu>
2017-11-17 12:14:30 -0800

    Add success flag to error response

--- standard_error.py
+++ standard_error.py
@@ -1,17 +1,18 @@
-from flask import jsonify
-
 class StandardError(Exception):
     status_code = 400
 
     def __init__(self, message, status_code=None, data=None):
         Exception.__init__(self)
         self.message = message
-        self.success = False
         if status_code is not None:
             self.status_code = status_code
         self.data = data
 
     def to_dict(self):
-        rv = dict(self.data or ())
-        rv['message'] = self.message
+        rv = {
+            'message': self.message,
+            'success': False
+        }
+        if self.data is not None:
+            rv['data'] = self.data
         return rv
